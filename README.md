b853f40b6f7e80187ec4cadbc164bf5776453af6
el-yagni <niqueepremiere@gmail.com>
2024-09-10 12:47:49 +0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Myapp
+# DI LUAR NALAR COMPANY
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.0.
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "about", component: AboutComponent},
+    {path: "product", component: ProductComponent},
+    {path: "**", component: NotfoundComponent},
+];
